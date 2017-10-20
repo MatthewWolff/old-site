6e46a76711092763cb2108bb041bc0bfbdc769ff
Matthew Wolff <mm.wolff@chi-squared.org>
2017-10-20 01:05:26 -0500

    Create matrix.js

---
+++ js/matrix.js
@@ -0,0 +1,81 @@
+    // https://codepen.io/riazxrazor/pen/Gjomdp
+    var canvas = document.getElementById( 'canvas' ),
+		ctx = canvas.getContext( '2d' ),
+    canvas2 = document.getElementById( 'canvas2' ),
+    ctx2 = canvas2.getContext( '2d' ),
+		// full screen dimensions
+		cw = window.innerWidth,
+		ch = window.innerHeight,
+    charArr = ['a','c','t','g','u'],
+    maxCharCount = 100,
+    fallingCharArr = [],
+    fontSize = 10,
+    maxColums = cw/(fontSize);
+    canvas.width = canvas2.width = cw;
+    canvas.height = canvas2.height = ch;
+    var change_letter = 0;
+
+
+    function randomInt( min, max ) {
+    	return Math.floor(Math.random() * ( max - min ) + min);
+    }
+
+    function randomFloat( min, max ) {
+    	return Math.random() * ( max - min ) + min;
+    }
+
+    function Point(x,y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+
+    Point.prototype.draw = function(ctx, chance){
+      if(chance % 19 == 0){
+      this.value = charArr[randomInt(0,charArr.length-1)].toUpperCase();
+      }
+      this.speed = randomFloat(0.2,3);
+
+      ctx2.fillStyle = "rgba(255,255,255,0.8)";
+      ctx2.font = fontSize+"px san-serif";
+      ctx2.fillText(this.value,this.x,this.y);
+
+        ctx.fillStyle = "#0F0";
+        ctx.font = fontSize+"px san-serif";
+        ctx.fillText(this.value,this.x,this.y);
+
+
+
+        this.y += this.speed;
+        if(this.y > ch)
+        {
+          this.y = randomFloat(-100,0);
+          this.speed = randomFloat(2,5);
+        }
+    }
+
+    for(var i = 0; i < maxColums ; i++) {
+      fallingCharArr.push(new Point(i*fontSize,randomFloat(-500,0)));
+    }
+
+
+    var update = function()
+    {
+
+    ctx.fillStyle = "rgba(0,0,0,0.05)"; // trail
+    ctx.fillRect(0,0,cw,ch);
+
+    ctx2.clearRect(0,0,cw,ch);
+
+      var i = fallingCharArr.length;
+
+      while (i--) {
+        fallingCharArr[i].draw(ctx, change_letter);
+        var v = fallingCharArr[i];
+        change_letter = change_letter++ % 19 // keep from changing too often
+      }
+
+      requestAnimationFrame(update);
+    }
+
+  update();
